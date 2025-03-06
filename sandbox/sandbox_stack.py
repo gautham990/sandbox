d26700dab4d365d9bcdec66d1979c8179562ea2b
gauthan1990 <gashe@eg.dk>
2025-03-06 08:45:36 +0000

    Add S3 bucket resource and output for SandboxStack

--- sandbox/sandbox_stack.py
+++ sandbox/sandbox_stack.py
@@ -1,19 +1,13 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
+from aws_cdk import Stack
+from aws_cdk import aws_s3 as s3
+from aws_cdk import RemovalPolicy
 from constructs import Construct
+from aws_cdk import CfnOutput
 
 class SandboxStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "SandboxQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        bucket = s3.Bucket(self, "SandboxBucket", bucket_name="sandbox-bucket-837648",removal_policy=RemovalPolicy.DESTROY) 
+        CfnOutput(self, "BucketArnOutput", value=bucket.bucket_arn)
