b7f295c103d8ef83502c47421839ff4120502d28
gauthan1990 <gautham990@gmail.com>
2025-03-07 06:33:26 +0000

    Refactor SandboxStack to use app name for resource naming and add EC2 instance

--- sandbox/sandbox_stack.py
+++ sandbox/sandbox_stack.py
@@ -9,20 +9,22 @@ class SandboxStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        app_name = "Sandbox"
 
+        #S3 Bucket
         bucket = s3.Bucket(self, "SandboxBucket", bucket_name="sandbox-bucket-837648",removal_policy=RemovalPolicy.DESTROY) 
         CfnOutput(self, "BucketArnOutput", value=bucket.bucket_arn)
-        vpc_name = "Sandbox"
-
-        vpc = ec2.Vpc(self, f"{vpc_name}",cidr="10.0.0.0/16",max_azs=2, 
+        
+        #VPC
+        vpc = ec2.Vpc(self, f"{app_name}",cidr="10.0.0.0/16",max_azs=2, 
             subnet_configuration=[
                 ec2.SubnetConfiguration(
-                    name=f"{vpc_name}-PublicSubnet",
+                    name=f"{app_name}-PublicSubnet",
                     subnet_type=ec2.SubnetType.PUBLIC,
                     cidr_mask=24
                 ),
                 ec2.SubnetConfiguration(
-                    name=f"{vpc_name}-PrivateSubnet",
+                    name=f"{app_name}-PrivateSubnet",
                     # subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS,
                     subnet_type=ec2.SubnetType.PRIVATE_ISOLATED,
                     cidr_mask=24
@@ -30,7 +32,13 @@ class SandboxStack(Stack):
             ],
             nat_gateways=0
         )
-        internetSecurityGroup = ec2.SecurityGroup(self, "InternetSecurityGroup", vpc=vpc, allow_all_outbound=True, security_group_name=f"{vpc_name}-InternetSecurityGroup")
+
+        #Secruity Groups
+        internetSecurityGroup = ec2.SecurityGroup(self, "InternetSecurityGroup", vpc=vpc, allow_all_outbound=True, security_group_name=f"{app_name}-InternetSecurityGroup")
         internetSecurityGroup.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(443), "Allow HTTPS from anywhere")
-        intranetSecurityGroup = ec2.SecurityGroup(self, "IntranetSecurityGroup", vpc=vpc, allow_all_outbound=True, security_group_name=f"{vpc_name}-IntranetSecurityGroup")
-        intranetSecurityGroup.add_ingress_rule(ec2.Peer.security_group_id(internetSecurityGroup.security_group_id), ec2.Port.all_traffic(), "Allow traffic from intranet SG")
+        intranetSecurityGroup = ec2.SecurityGroup(self, "IntranetSecurityGroup", vpc=vpc, allow_all_outbound=True, security_group_name=f"{app_name}-IntranetSecurityGroup")
+        intranetSecurityGroup.add_ingress_rule(ec2.Peer.security_group_id(internetSecurityGroup.security_group_id), ec2.Port.all_traffic(), "Allow traffic from internet SG")
+
+        #EC2 Instance
+        instance = ec2.Instance(self, "SandboxInstance", instance_type=ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO), machine_image=ec2.MachineImage.latest_amazon_linux(), instance_name=f"{app_name}Instance",  vpc=vpc, vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_ISOLATED), security_group=intranetSecurityGroup)
+        CfnOutput(self, "InstanceOutput", value=instance.instance_id)
